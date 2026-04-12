74f91dfed07d398bdf36004a7985b858a8e29d78
Marcos Vinicius Ferreira Paim <viniciusferreirapaim@yahoo.com>
2026-04-12 16:09:18 -0300

    Removendo datasets pesados e configurando gitignore

---
+++ Predicao_Sucesso_Titulo.py
@@ -0,0 +1,5 @@
+# %% [markdown]
+# # Análise Preditiva: Títulos Steam 2025
+# Uma publicadora de jogos usaria isso para decidir se investe em um novo projeto antes de ele ser lançado.
+print("hello word")
+# %%
